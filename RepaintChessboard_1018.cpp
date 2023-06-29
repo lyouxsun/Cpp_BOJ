18df51e2e9402067d2e87afd2adb3f881a8f01e2
lyouxsun <leedrkr323@konkuk.ac.kr>
2023-06-29 15:33:44 +0900

    initial commit

---
+++ RepaintChessboard_1018.cpp
@@ -0,0 +1,61 @@
+//함수형 프로그래밍ㄱㄱ!!!!!
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int board[50][50];
+int cnt=0;
+
+int BW(int x, int y){   //x,y는 8*8의 시작점
+    for(int i=0;i<8;i++){
+        for (int j=0;j<8;j++){
+            if((i+j)%2==0){
+                if(board[i][j]!=1) cnt++;
+            }
+            else{
+                if(board[i][j]!=0) cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int WB(int x, int y){   //x,y는 8*8의 시작점
+    for(int i=0;i<8;i++){
+        for (int j=0;j<8;j++){
+            if((i+j)%2==0){
+                if(board[i][j]!=0) cnt++;
+            }
+            else{
+                if(board[i][j]!=1) cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int a, b, tempAnswer;    
+    int answer=100000000;
+    cin >> a >> b;
+    char tmp;
+    board[a][b];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            cin>>tmp;
+            if(tmp=='B'){
+                board[i][j]=1;
+            }else board[i][j]=0;
+        }
+    }
+
+    for(int i=0;i+8<=a;i++){
+        for(int j=0;j+8<=b;j++){
+            tempAnswer=min(BW(i,j), WB(i,j));
+        }
+        answer=min(answer, tempAnswer);
+    }
+    cout<<answer;
+}
